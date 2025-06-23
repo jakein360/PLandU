284a7b65b57f84e9b1097f8bf2497279e52b8d5e
Jake Emitte <jakeemitte829@gmail.com>
2025-06-22 18:10:14 -0700

    took previous lessons and mad an app to toggle between lyrics in the song with a corrisponding image.

--- PLandU/ContentView.swift
+++ PLandU/ContentView.swift
@@ -8,12 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
+   
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            Spacer()
+            
+           Image(systemName: imageName)
+                .resizable()
+                .frame(width: 300, height: 300)
+                .foregroundStyle(.black)
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.purple)
+            
+            Spacer()
+            
+            HStack {
+                Button("Peace") {
+                    imageName = "peacesign"
+                    message = "Peace"
+                }
+                Button("Love") {
+                    imageName = "heart"
+                    message = "Love"
+                }
+                Button("Understanding") {
+                    imageName = "lightbulb"
+                    message = "Understanding"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+         
         }
         .padding()
     }
