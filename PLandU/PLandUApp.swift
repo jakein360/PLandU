58d2b1967a9c2298e23018ad358b00d7f9769985
Jake Emitte <jakeemitte829@gmail.com>
2025-06-22 17:30:34 -0700

    Initial Commit

---
+++ PLandU/PLandUApp.swift
@@ -0,0 +1,17 @@
+//
+//  PLandUApp.swift
+//  PLandU
+//
+//  Created by Jake Emitte on 6/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PLandUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
